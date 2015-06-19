108057fbad963fe32f12bb273dcce4c06cb5ee59
ngboonpin <ngboonpin@gmail.com>
2015-06-20 00:20:37 +0800

    Code and comments added to file

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions receive special matrix, calculate and cache its inverse.
 
-## Write a short comment describing this function
+##  makeCacheMatrix creates a list containing a function to
+##  1. set the value of the matrix
+##  2. get the value of the matrix
+##  3. set the value of inverse of the matrix
+##  4. get the value of inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    
+    #   Define function to set value of the matrix
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    
+    #   Define function to get value of the matrix
+    get <- function() x
+    
+    #   Define function to set inverse
+    setInverse <- function(inverse) m <<- inverse
+    
+    #   Define function to get inverse
+    getInverse <- function() m
+    
+    #   Return a list with the above four functions
+    list(set = set, get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
+#   This function computes the inverse matrix returned by the makeCacheMatrix.
+#   If the inverse is calculated and no changed in value, this function will 
+#   return the cache value.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    #   Fetches the cache value for inverse
+    m <- x$getInverse()
+    
+    #   If there is value in cache, it will give message "getting cache data" 
+    #   and return the inverse value
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    
+    #   If no value return, it will compute the value and cache it
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setInverse(m)
+    m
 }
